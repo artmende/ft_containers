6e45e6784d6efeaf82062220b966920e8e45610a
artmende <artmende@student.s19.be>
2022-06-01 11:50:06 +0200

    building constructors

--- testfile.cpp
+++ testfile.cpp
@@ -6,7 +6,7 @@
 /*   By: artmende <artmende@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 13:52:27 by artmende          #+#    #+#             */
-/*   Updated: 2022/05/31 16:54:53 by artmende         ###   ########.fr       */
+/*   Updated: 2022/06/01 11:43:56 by artmende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	main()
 
 //	ClapTrap	b("Coucou");
 
-	std::vector<ClapTrap>	v;
+/*	std::vector<ClapTrap>	v;
 
 	ft::vector<int>	vi;
 
@@ -37,7 +37,36 @@ int	main()
 	{
 		std::cout << "Capacity : " << v.capacity() << " | Size : " << v.size() << std::endl;
 		v.push_back(ClapTrap());
+	}*/
+
+/*
+	std::allocator<ClapTrap>	al;
+
+	ClapTrap	*ar = al.allocate(5);
+
+	ClapTrap const & val = ClapTrap("yop");
+
+	for (size_t i = 0; i < 5; i++)
+	{
+		al.construct(ar + i, val);
 	}
+
+	std::cout << ar[3] << std::endl;
+
+*/
+
+
+
+	ft::vector<ClapTrap>	v(20, ClapTrap("yop"));
+
+	ClapTrap	*ar = (ClapTrap *)v.get();
+
+	for (size_t i = 0; i < 20; i++)
+	{
+		std::cout << *(ar + i) << std::endl;
+	}
+
+
 
 /*
 	v.resize(10, b);
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: artmende <artmende@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 15:51:52 by artmende          #+#    #+#             */
-/*   Updated: 2022/05/31 16:48:46 by artmende         ###   ########.fr       */
+/*   Updated: 2022/06/01 11:46:09 by artmende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,56 @@
 
 namespace	ft
 {
-
-	template<typename value_type>
+	template<typename T>
 	class vector
 	{
-	private:
-		value_type*	_values;
-		size_t		_size;
 	public:
-		vector()
+		typedef T value_type;
+
+/*		explicit vector(const allocator_type& alloc = allocator_type()) // default constructor
 		{}
-		~vector(){}
+		explicit vector(size_type n, const value_type& val = value_type(), // fill constructor
+			const allocator_type& alloc = allocator_type())
+		{}
+		template <class InputIterator>
+			vector(InputIterator first, InputIterator last, // range constructor
+				const allocator_type& alloc = allocator_type())
+		{}
+		vector(const vector& x) // copy constructor
+		{}*/
+
+		vector() : _values(NULL) {} // default constructor
+		vector(size_t n, const value_type& val = value_type())
+		{
+			std::allocator<value_type>	al;
+			this->_values = al.allocate(n);
+			for (size_t i = 0; i < n; i++)
+			{
+				al.construct(this->_values + i, val);
+			}
+			this->_size = n;
+		}
+
+
+
+		~vector()
+		{
+			std::allocator<value_type>	al;
+			al.deallocate(this->_values, this->_size);
+		}
 
 		void	push_back(const value_type& val)
 		{
 			
 		}
+		void	*get()
+		{
+			return ((void *)this->_values);
+		}
+	private:
+		value_type*	_values;
+		size_t		_size;
 	};
-
 }
 
 #endif
